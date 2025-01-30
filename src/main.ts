c5bbef7b0c4c5ffcf3899034fb92069b0771bf09
hebafaisal <hebafaisalalsharif5@gmail.com>
2025-01-30 14:10:49 +0300

    updata

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(cors({
+    origin: '*', // Allow all origins (for development)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
